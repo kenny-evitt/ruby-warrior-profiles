8b1d142217ae61de60a85142a8a69e8f8a521e2e
Kenny Evitt <kenny.evitt@gmail.com>
2015-11-19 23:51:08 -0500

    WIP for level 6

--- markus-beginner/player.rb
+++ markus-beginner/player.rb
@@ -13,21 +13,71 @@ class Player
 
   def initialize
     @health_last_turn = nil
+    @is_backward = true
   end
 
-  def was_attacked(warrior)
-    warrior.health < (@health_last_turn || 0)
+  def attack!
+    if @is_backward
+      @current_warrior.attack! :backward
+    else
+      @current_warrior.attack!
+    end
+  end
+
+  def can_survive_any_attack?
+    health >= Player.min_health_to_survive_attack
+  end
+
+  def feel
+    if @is_backward
+      @current_warrior.feel :backward
+    else
+      @current_warrior.feel
+    end
+  end
+
+  def health
+    @current_warrior.health
+  end
+
+  def rescue!
+    if @is_backward
+      @current_warrior.rescue! :backward
+    else
+      @current_warrior.rescue!
+    end
+  end
+
+  def rest!
+    @current_warrior.rest!
+  end
+
+  def was_attacked?
+    health < (@health_last_turn || 0)
+  end
+
+  def walk!
+    if @is_backward
+      @current_warrior.walk! :backward
+    else
+      @current_warrior.walk!
+    end
   end
 
   def play_turn(warrior)
-    if current_space.captive?
-      warrior.rescue!
-    elsif warrior.health < Player.min_health_to_survive_attack && !was_attacked(warrior)
-      warrior.rest!
-    elsif warrior.feel.empty?
-      warrior.walk!
+    @current_warrior = warrior
+    next_space = feel
+
+    if next_space.wall?
+      @is_backward = false
+    elsif next_space.captive?
+      rescue!
+    elsif !can_survive_any_attack? && !was_attacked?
+      rest!
+    elsif next_space.empty?
+      walk!
     else
-      warrior.attack!
+      attack!
     end
 
     @health_last_turn = warrior.health
