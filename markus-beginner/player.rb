da944030a79951ef7a57f87e2418f8c8145d39d3
Kenny Evitt <kenny.evitt@gmail.com>
2015-11-19 23:29:59 -0500

    Add class method to calculate minimum health needed to survive attacking any enemy (encountered so far) and modify  method to rest until that minimum health is restored (unless the warrior is currently being attacked).

--- markus-beginner/player.rb
+++ markus-beginner/player.rb
@@ -1,5 +1,15 @@
 class Player
   MIN_TOTAL_ATTACKING_DAMAGE_SLUDGE = 6
+  MIN_TOTAL_ATTACKING_DAMAGE_THICK_SLUDGE = 12
+  MIN_TOTAL_ATTACKING_DAMAGE_ARCHER = 6
+
+  def self.min_health_to_survive_attack
+    [
+      MIN_TOTAL_ATTACKING_DAMAGE_SLUDGE,
+      MIN_TOTAL_ATTACKING_DAMAGE_THICK_SLUDGE,
+      MIN_TOTAL_ATTACKING_DAMAGE_ARCHER
+    ].max + 1
+  end
 
   def initialize
     @health_last_turn = nil
@@ -10,7 +20,7 @@ class Player
   end
 
   def play_turn(warrior)
-    if warrior.health < (MIN_TOTAL_ATTACKING_DAMAGE_SLUDGE + 1) && !was_attacked(warrior)
+    if warrior.health < Player.min_health_to_survive_attack && !was_attacked(warrior)
       warrior.rest!
     elsif warrior.feel.empty?
       warrior.walk!
